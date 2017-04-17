4ca8689cb00d6aa0d5b71fdb823feabc6c2b23eb
HenryHsin <hooy123456@gmail.com>
2017-04-17 13:36:57 +0800

    commit

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,12 +2,22 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get("fbwebhook") { request in
+    print("get webhook")
+    guard let token = request.data["hub.verify_token"]?.string else {
+        throw Abort.badRequest
+    }
+    guard let response = request.data["hub.challenge"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    if token == "2318934571" {
+        print("send response")
+        return response(status: .ok, body: response)
+    } else {
+        return response(status: .ok, body: "Error, invalid token")
+    }
 }
 
-drop.resource("posts", PostController())
-
 drop.run()
+
